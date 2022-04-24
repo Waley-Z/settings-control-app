46b38490c22993abfee3b6b0491e7b3b7c0fe0ae
Waley-Z <waley_zheng@sjtu.edu.cn>
2022-04-23 20:53:50 -0400

    Update style

--- app/src/main/java/com/example/settingscontrolapp/MainActivity.java
+++ app/src/main/java/com/example/settingscontrolapp/MainActivity.java
@@ -30,12 +30,14 @@ public class MainActivity extends AppCompatActivity {
         getSettingsWritePermission();
         setContentView(R.layout.activity_main);
 
+        // components
         Slider brightnessSlider = findViewById(R.id.brightnessSlider);
         Slider volumeSlider = findViewById(R.id.volumeSlider);
         Switch nightSwitch = findViewById(R.id.nightSwitch);
         Switch grayscaleSwitch = findViewById(R.id.grayscaleSwitch);
         Slider animationSlider = findViewById(R.id.animationSlider);
 
+        // brightness
         brightnessSlider.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
             @Override
             public void onStartTrackingTouch(Slider slider) {
@@ -50,17 +52,16 @@ public class MainActivity extends AppCompatActivity {
                         final Handler handler = new Handler();
                         handler.postDelayed(() -> {
                             modifyBrightness(MainActivity.this, Math.round(slider.getValue() / 100 * 180));
-                            Toast.makeText(MainActivity.this, "System brightness: " + getScreenBrightness(MainActivity.this),
-                                    Toast.LENGTH_SHORT).show();
+                            toast("System brightness: " + getScreenBrightness(MainActivity.this));
                         }, 0); // delay 5 seconds
                     } else {
-                        Toast.makeText(MainActivity.this, "You have declined to grant permission.",
-                                Toast.LENGTH_SHORT).show();
+                        toast("You have declined to grant permission.");
                     }
                 }
             }
         });
 
+        // volume
         volumeSlider.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
             @Override
             public void onStartTrackingTouch(Slider slider) {
@@ -71,34 +72,37 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(Slider slider) {
                 Log.d("PRINT", "" + slider.getValue());
                 modifyVolume(slider.getValue() / 100);
-                Toast.makeText(MainActivity.this, "System media volume: " + getVolume(),
-                        Toast.LENGTH_SHORT).show();
+                toast("System media volume: " + getVolume());
             }
         });
 
+        // night mode
         nightSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             UiModeManager ui = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
             if (isChecked) {
                 ui.setNightMode(UiModeManager.MODE_NIGHT_YES);
-                Toast.makeText(MainActivity.this, "Night mode enabled.", Toast.LENGTH_SHORT).show();
+                toast("Night mode enabled.");
             } else {
                 ui.setNightMode(UiModeManager.MODE_NIGHT_NO);
-                Toast.makeText(MainActivity.this, "Night mode disabled.", Toast.LENGTH_SHORT).show();
+                toast("Night mode disabled.");
             }
         });
 
+        // grayscale
         grayscaleSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             UiModeManager ui = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
             if (isChecked) {
                 Settings.Secure.putString(this.getContentResolver(), "accessibility_display_daltonizer_enabled", "1");
                 Settings.Secure.putString(this.getContentResolver(), "accessibility_display_daltonizer", "0");
+                toast("Grayscale mode enabled.");
             } else {
                 Settings.Secure.putString(this.getContentResolver(), "accessibility_display_daltonizer_enabled", "0");
                 Settings.Secure.putString(this.getContentResolver(), "accessibility_display_daltonizer", "-1");
-                Toast.makeText(MainActivity.this, "Night mode disabled.", Toast.LENGTH_SHORT).show();
+                toast("Grayscale mode disabled.");
             }
         });
 
+        // animation scale
         animationSlider.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
             @Override
             public void onStartTrackingTouch(Slider slider) {
@@ -110,14 +114,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("PRINT", "" + slider.getValue());
                 Settings.Global.putString(MainActivity.this.getContentResolver(), "window_animation_scale", String.valueOf(slider.getValue()));
                 Settings.Global.putString(MainActivity.this.getContentResolver(), "transition_animation_scale", String.valueOf(slider.getValue()));
-                Toast.makeText(MainActivity.this, "System animation scale: " +
-                                Settings.Global.getString(MainActivity.this.getContentResolver(), "window_animation_scale"),
-                        Toast.LENGTH_SHORT).show();
+                toast("System animation scale: " +
+                        Settings.Global.getString(MainActivity.this.getContentResolver(), "window_animation_scale"));
             }
         });
     }
 
-    public void setScreenManualMode(Context context) {
+    // create Toast message
+    private void toast(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    // set screen mode to manual to prevent automatic change
+    private void setScreenManualMode(Context context) {
         ContentResolver contentResolver = context.getContentResolver();
         try {
             int mode = Settings.System.getInt(contentResolver,
@@ -134,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_WRITE_SETTINGS = 1000;
 
+    // ask permission to write system settings
     private void getSettingsWritePermission() {
         // Settings.System.canWrite(MainActivity.this)
         // Checks whether permission to change system settings is granted
@@ -143,10 +153,7 @@ public class MainActivity extends AppCompatActivity {
             builder.setMessage("Please grant permission to change system settings.");
 
             // user click no
-            builder.setNegativeButton(android.R.string.no,
-                    (dialog, which) -> Toast.makeText(MainActivity.this,
-                            "Permission denied.",
-                            Toast.LENGTH_SHORT).show());
+            builder.setNegativeButton(android.R.string.no, (dialog, which) -> toast("Permission denied."));
 
             ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
                     new ActivityResultContracts.StartActivityForResult(),
@@ -169,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // get current screen brightness
     private int getScreenBrightness(Context context) {
         ContentResolver contentResolver = context.getContentResolver();
         int defVal = 125;
@@ -176,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
                 Settings.System.SCREEN_BRIGHTNESS, defVal);
     }
 
+    // change brightness
     private void modifyBrightness(Context context, int brightnessValue) {
         // Change screen settings to manual mode
         setScreenManualMode(context);
@@ -184,18 +193,18 @@ public class MainActivity extends AppCompatActivity {
         Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightnessValue);
     }
 
+    // get current volume
     private int getVolume() {
         AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         return audio.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
 
+    // change volume
     private void modifyVolume(float volumePercentage) {
         AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         int streamMaxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         if (audio.isVolumeFixed()) {
-            Toast.makeText(MainActivity.this,
-                    "System volume is fixed.",
-                    Toast.LENGTH_SHORT).show();
+            toast("System volume is fixed.");
         } else {
             audio.setStreamVolume(AudioManager.STREAM_MUSIC, Math.round(volumePercentage * streamMaxVolume), AudioManager.FLAG_SHOW_UI);
         }
